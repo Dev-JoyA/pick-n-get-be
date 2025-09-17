9812a6c8c1c9d810439cf90ddcb4f6cbc91d3bfa
Joy A <joy.gold13@gmail.com>
2025-09-17 21:05:30 +0100

    delivery flow v1 done

--- server.ts
+++ server.ts
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import {sequelize, client}  from "./src/config/db.ts"
+import {sequelize}  from "./src/config/db.ts"
+import route from "./src/routes/deliveryRoute.ts"
 
 dotenv.config();
 
@@ -14,20 +15,13 @@ try {
   console.error('Unable to connect to the database:', error);
 }
 
-client.on('error', err => console.log('Redis Client Error', err));
-
-await client.connect();
-console.log("redis sucessfully connected", client);
-
 const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.get("/", (req : Request, res : Response) => {
-    res.json({message : "It works"});
-})
+app.use("/api/v1", route)
 
 
 
---
+++ src/routes/deliveryRoute.ts
@@ -0,0 +1,10 @@
+import express, {Router} from "express"
+import {validateR, updateStatus, pickRide} from "../controlllers/deliveryController"
+
+const route = Router();
+
+route.post("/pick-ride/:user/:item", pickRide)
+route.post("/validate-ride/:rider/:pick-up-id", validateR)
+route.post("/update-status/:rider/:pick-up-id", updateStatus)
+
+export default route;
